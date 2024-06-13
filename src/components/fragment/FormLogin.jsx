cc7ad9454d47dd6fd7f9163e2b2b8990422342ca
Hassanjadi <hassanjadi25@gmail.com>
2024-06-13 20:51:47 +0700

    feat: login local storage

--- src/components/fragment/FormLogin.jsx
+++ src/components/fragment/FormLogin.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Button } from "../Element/Button";
 import { InputForm } from "../Element/Input/Index";
 
 export const FormLogin = () => {
+  const handleLogin = () => {
+    event.preventDefault();
+
+    localStorage.setItem("email", event.target.email.value);
+    localStorage.setItem("password", event.target.password.value);
+    window.location.href = "/";
+  };
   return (
-    <form className="d-flex flex-column gap-3">
+    <form className="d-flex flex-column gap-3" onSubmit={handleLogin}>
       <InputForm
         label="Email"
         type="email"
@@ -16,7 +24,9 @@ export const FormLogin = () => {
         name="password"
         placeholder="6+ Karakter"
       />
-      <button type={"submit"}>Submit</button>
+      <Button classname="btn-login" type="submit">
+        Login
+      </Button>
     </form>
   );
 };
