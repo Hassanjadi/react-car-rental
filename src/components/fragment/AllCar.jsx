2a2d0e638f6ebe54d48e8707222703c5a37eca0f
Hassanjadi <hassanjadi25@gmail.com>
2024-06-14 18:56:10 +0700

    refactor

--- src/components/fragment/AllCar.jsx
+++ src/components/fragment/AllCar.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { CardCar } from "./CardCar";
+import cars from "../../assets/data/cars.json";
 
 export const AllCar = () => {
+  const handleRupiah = (number) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(number);
+  };
+
   return (
-    <main>
-      <section id="all-cars" className="all-cars">
-        <div className="container-md">
-          <div className="row d-flex justify-content-center">
-            <div className="cars">
-              <CardCar />
-            </div>
-          </div>
+    <section id="all-cars" className="all-cars">
+      <div className="container-md">
+        <div className="cars">
+          {cars.map((item) => (
+            <CardCar key={item.id}>
+              <CardCar.Header image={item.image} />
+              <CardCar.Body
+                name={item.manufacture}
+                type={item.type}
+                price={handleRupiah(item.rentPerDay)}
+                description={item.description}
+                capacity={item.capacity}
+                transmission={item.transmission}
+                year={item.year}
+              />
+              <CardCar.Footer />
+            </CardCar>
+          ))}
         </div>
-      </section>
-    </main>
+      </div>
+    </section>
   );
 };
--- src/components/fragment/CardCar.jsx
+++ src/components/fragment/CardCar.jsx
@@ -1,49 +1,64 @@
 import React from "react";
 import FeatherIcon from "feather-icons-react";
-import cars from "../../assets/data/cars.json";
 
-export const CardCar = () => {
+export const CardCar = (props) => {
+  const { children } = props;
   return (
-    <div className="col cars-container">
-      {cars.map((item) => (
-        <div className="card col w-100" key={item.id}>
-          <div className="d-flex flex-column justify-content-between h-100">
-            <div className="card-images d-flex justify-content-center align-items-center w-100">
-              <img src={item.image} alt="Images car" className="images-car" />
-            </div>
-            <div className="d-flex flex-column gap-2 my-4">
-              <div className="card-descriptions">
-                <p className="name">
-                  {item.manufacture}/{item.type}
-                </p>
-                <p className="price">Rp. {item.rentPerDay}/ Hari</p>
-                <p className="desc h-50">{item.description}</p>
-              </div>
-              <div className="card-info d-flex flex-column gap-2">
-                <div className="d-flex gap-3 justify-content-start">
-                  <span>
-                    <FeatherIcon icon="users" />
-                  </span>
-                  <p>{item.capacity} Orang</p>
-                </div>
-                <div className="d-flex gap-3 justify-content-start">
-                  <span>
-                    <FeatherIcon icon="settings" />
-                  </span>
-                  <p>{item.transmission}</p>
-                </div>
-                <div className="d-flex gap-3 justify-content-start">
-                  <span>
-                    <FeatherIcon icon="calendar" />
-                  </span>
-                  <p>Tahun {item.year}</p>
-                </div>
-              </div>
-            </div>
-            <button>Pilih Mobil</button>
-          </div>
-        </div>
-      ))}
+    <div className="card d-flex flex-column justify-content-between h-100 w-100">
+      {children}
     </div>
   );
 };
+
+const Header = (props) => {
+  const { image } = props;
+  return (
+    <div className="card-images d-flex justify-content-center align-items-center w-100">
+      <img src={image} alt="Images car" className="images-car" />
+    </div>
+  );
+};
+
+const Body = (props) => {
+  const { name, type, price, description, capacity, transmission, year } =
+    props;
+  return (
+    <div className="d-flex flex-column gap-3 my-3">
+      <div className="card-descriptions">
+        <p className="name">
+          {name}/{type}
+        </p>
+        <p className="price">{price} / hari</p>
+        <p className="desc h-50">{description}</p>
+      </div>
+      <div className="card-info d-flex flex-column gap-2">
+        <div className="d-flex gap-3 justify-content-start">
+          <span>
+            <FeatherIcon icon="users" />
+          </span>
+          <p>{capacity} Orang</p>
+        </div>
+        <div className="d-flex gap-3 justify-content-start">
+          <span>
+            <FeatherIcon icon="settings" />
+          </span>
+          <p>{transmission}</p>
+        </div>
+        <div className="d-flex gap-3 justify-content-start">
+          <span>
+            <FeatherIcon icon="calendar" />
+          </span>
+          <p>Tahun {year}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <button>Pilih Mobil</button>;
+};
+
+CardCar.Header = Header;
+CardCar.Body = Body;
+CardCar.Footer = Footer;
